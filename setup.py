19b7176851ef4b32cfbcba947b6bdb0d2cdc1e22
yvan <yvanscher@gmail.com>
2016-05-06 16:33:15 -0400

    repalaced smappy with pysmap

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(name='pysmap',
 	packages=['pysmap', 'pysmap.twitterutil', 'pysmap.ntwrk', 'pysmap.viz'],
 	version='0.0.2',
-	description='pysmapp is a set of tools for working with twitter data',
+	description='pysmap is a set of tools for working with twitter data',
 	author='yvan',
 	author_email='yns207@nyu.edu',
 	url='https://github.com/SMAPPNYU/pysmapp',
-	keywords='twitter data tools pysmapp',
+	keywords='twitter data tools pysmap',
 	license='MIT',
 	install_requires=[
 	  'smappdragon>=0.0.21'
