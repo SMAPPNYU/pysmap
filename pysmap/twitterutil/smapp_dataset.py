82ec104370d53ead643a0dd87dcf003ade4a9fb1
yvan <yvanscher@gmail.com>
2016-12-01 17:59:30 -0500

    made ti so that parallel dumps dont omit header

--- pysmap/twitterutil/smapp_dataset.py
+++ pysmap/twitterutil/smapp_dataset.py
@@ -246,7 +246,7 @@ class SmappDataset(object):
         for i, collection in enumerate(self.collections):
             if parallel:
                 filename, file_extension = output_file.split(os.extsep, 1)
-                collection.dump_to_csv('{}_{}.{}'.format(filename, i, file_extension), keep_fields, write_header=(i == 0))
+                collection.dump_to_csv('{}_{}.{}'.format(filename, i, file_extension), keep_fields)
             else:
                 collection.dump_to_csv(output_file, keep_fields, write_header=(i == 0))
 
